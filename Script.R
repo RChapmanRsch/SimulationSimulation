e1e9293981333205f3578eb84c19fccd4d497c45
RChapmanRsch <chapm564@umn.edu>
2024-05-23 12:00:32 -0500

    RC coding 5/23

--- Script.R
+++ Script.R
@@ -1,1 +1,46 @@
-#testlrkjas;dlkjah;sdlkfjads
+#Script for SimulationSimulation Study with Raj-yRajRaj
+
+#load packages
+
+  #Analysis Methods
+  #Random Forest: Ranger in "ranger" package
+  install.packages("ranger")
+  library(ranger)
+
+  #CART: conditional inference trees in "partykit" package
+  install.packages("partykit")
+  library(partykit)
+  
+  #Lasso: glmnet in 'glmnet' package
+  install.packages("glmnet")
+  library(glmnet)
+  
+  #smoothing splines??
+
+
+
+#simulation conditions
+Conditions=list(
+  #1) Sample Size
+  
+  "SampleSize"=c(outer(c(1,5),10^(2:6),FUN="*")), #100 to 5 millie in 10 steps
+  
+  #2) Factor Characteristics
+  #2a) Number of Factors,
+  "Number_of_Factors"=2^(1:6), #2 to 64 in 6 steps
+  #2b) Factor Levels & 
+  "Factor_Levels"=2^(2^(0:4)), #dichotomous to highly granular (e.g., continuous)
+  #2c) Percent of Unimportant Factors 
+  "Percent_Unimportant_Factors"= c(0,25,50,100), #0 (no unimportant) to 100% none of the factors are related to the outcome
+  #3) Structure of Factors (independence/interactions)
+  "Factor_Structure"=c("independent_zero_interrelations, uniform true variable importance","dependence/interaction between factors, skewed true variable importance"),
+  #4) Distribution of Outcome
+  "Distribution of Outcome"=c("Dichotomous","Categorical","ContinuousTightSD","ContinuousLargeSD"))
+
+Conditions=expand.grid(Conditions)
+
+#generate data
+
+
+
+
